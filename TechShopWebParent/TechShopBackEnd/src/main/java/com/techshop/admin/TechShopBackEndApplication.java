ca5599f9caa33522a5c8bd0f3deea10ea7cb8365
Huy Quang <huyquang271099@gmail.com>
2022-02-03 23:11:28 +0700

    tech_shop_e_commerce

---
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/TechShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.techshop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechShopBackEndApplication.class, args);
+	}
+
+}
---
+++ TechShopWebParent/TechShopFrontEnd/src/main/java/com/techshop/site/TechShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.techshop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechShopFrontEndApplication.class, args);
+	}
+
+}
