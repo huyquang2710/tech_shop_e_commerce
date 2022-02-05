3687395805a036ef0604859d579b64d947afac72
Huy Quang <huyquang271099@gmail.com>
2022-02-05 15:32:10 +0700

    Role entity, role repo, unit test

--- TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/TechShopBackEndApplication.java
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/TechShopBackEndApplication.java
@@ -2,8 +2,10 @@ package com.techshop.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.techshop.common.entity", "com.techshop.admin.user"})
 public class TechShopBackEndApplication {
 
 	public static void main(String[] args) {
