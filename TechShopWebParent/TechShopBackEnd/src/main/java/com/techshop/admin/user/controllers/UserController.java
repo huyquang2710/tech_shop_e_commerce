09b3c038de116260eb8c74d8a49471e6114e12aa
Huy Quang <huyquang271099@gmail.com>
2022-02-15 07:21:12 +0700

    create new user function

--- TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/controllers/UserController.java
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/controllers/UserController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.techshop.admin.user.services.UserService;
+import com.techshop.common.entity.Role;
 import com.techshop.common.entity.User;
 
 @Controller
@@ -20,5 +23,25 @@ public class UserController {
 		List<User> userList = userService.findAll();
 		model.addAttribute("user", userList);
 		return "users";
+	}
+	@GetMapping("/users/new")
+	public String user_form(Model model) {
+		List<Role> roleList = userService.listRole();
+		
+		User user = new User();
+		
+		user.setEnabled(true);
+		model.addAttribute("user", user);
+		model.addAttribute("rolesList", roleList);
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
+		userService.saveUser(user);	
+		return "redirect:/users";
 	}
 }
--- TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/services/UserService.java
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/services/UserService.java
@@ -5,15 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.techshop.admin.user.repositories.RoleRepository;
 import com.techshop.admin.user.repositories.UserRepository;
+import com.techshop.common.entity.Role;
 import com.techshop.common.entity.User;
 
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	public List<User> findAll() {
 		return (List<User>) userRepository.findAll();
+	}
+	public List<Role> listRole() {
+		return (List<Role>) roleRepository.findAll();
+	}
+	public User saveUser(User user) {
+		return userRepository.save(user);
 	}
 }
