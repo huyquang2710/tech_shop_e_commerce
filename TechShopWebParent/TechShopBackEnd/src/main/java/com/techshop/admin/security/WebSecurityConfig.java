c3a7060a68e2556f40dda58847afe2c3883d445d
Huy Quang <huyquang271099@gmail.com>
2022-03-24 04:40:19 +0700

    basic authorize

--- TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/security/WebSecurityConfig.java
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/security/WebSecurityConfig.java
@@ -41,6 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/users/**").hasAuthority("Admin")
 		.anyRequest().authenticated()
 		.and()
 		.formLogin()
@@ -59,6 +60,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configure(WebSecurity web) throws Exception {
 	    web
 	        .ignoring()
-	        .antMatchers("/css*", "/fontawesome*", "/webjars/**", "/images/**", "/js/**");
+	        .antMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 }
