ecc31460a7f32fea424466f36bcebe7f1bc3688d
Huy Quang <huyquang271099@gmail.com>
2022-04-24 16:42:01 +0700

    Brand Service

--- TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/services/BrandService.java
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/services/BrandService.java
@@ -2,8 +2,13 @@ package com.techshop.admin.user.services;
 
 import java.util.List;
 
+import com.techshop.admin.exception.BrandNotFoundException;
 import com.techshop.common.entity.Brand;
 
 public interface BrandService {
 	List<Brand> findAll();
+	Brand save(Brand brand);
+	Brand findById(Integer id) throws BrandNotFoundException;
+	void delete(Integer id) throws BrandNotFoundException;
+	
 }
--- TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/services/impl/BrandServiceImpl.java
+++ TechShopWebParent/TechShopBackEnd/src/main/java/com/techshop/admin/user/services/impl/BrandServiceImpl.java
@@ -1,10 +1,12 @@
 package com.techshop.admin.user.services.impl;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.techshop.admin.exception.BrandNotFoundException;
 import com.techshop.admin.user.repositories.BrandRepository;
 import com.techshop.admin.user.services.BrandService;
 import com.techshop.common.entity.Brand;
@@ -18,6 +20,31 @@ public class BrandServiceImpl implements BrandService{
 	@Override
 	public List<Brand> findAll() {
 		return (List<Brand>) brandRepository.findAll();
+	}
+
+	@Override
+	public Brand save(Brand brand) {
+		return brandRepository.save(brand);
+	}
+
+	@Override
+	public Brand findById(Integer id) throws BrandNotFoundException {
+		try {
+			return brandRepository.findById(id).get();
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+			throw new BrandNotFoundException("Count not found any brand");
+		}
+	}
+
+	@Override
+	public void delete(Integer id) throws BrandNotFoundException {
+		Long countId = brandRepository.countById(id);
+		
+		if(countId == 0 || countId == null ) {
+			throw new BrandNotFoundException("Count not found any brand");
+		}
+		brandRepository.deleteById(id);
 	}
 	
 }
