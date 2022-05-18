7e90e77ca24ac1a0e1cb8f1dcf2f7690fcdd9906
Huy Quang <huyquang271099@gmail.com>
2022-05-19 01:18:40 +0700

    add extraImage, use JS

--- TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/common_form.js
+++ TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/common_form.js
@@ -1,20 +1,14 @@
-$(document).ready(function() {
-	$("#buttonCancel").on("click", function() {
+$(document).ready(function () {
+	$("#buttonCancel").on("click", function () {
 		window.location = moduleURL;
 	});
 	// size image
 	// show image
-	$("#fileImage").change(function() {
-		fileSize = this.files[0].size;
-		//alert("File size: " + fileSize);
-
-		if (fileSize > 110000000000) {
-			this.setCustomValidity("You must chooes an image less than 1MB!");
-			this.reportValidity();
-		} else {
-			this.setCustomValidity("");
-			showImageThumbnail(this);
+	$("#fileImage").change(function () {
+		if (!checkFileSize(this)) {
+			return;
 		}
+		showImageThumbnail(this);
 	});
 });
 
@@ -23,10 +17,24 @@ $(document).ready(function() {
 function showImageThumbnail(fileInput) {
 	var file = fileInput.files[0];
 	var reader = new FileReader();
-	reader.onload = function(e) {
+	reader.onload = function (e) {
 		$("#thumbnail").attr("src", e.target.result);
 	};
 	reader.readAsDataURL(file);
+}
+
+function checkFileSize(fileInput) {
+	fileSize = fileInput.files[0].size;
+	//alert("File size: " + fileSize);
+
+	if (fileSize > 50240000) {
+		fileInput.setCustomValidity("You must chooes an image less than 1MB!");
+		fileInput.reportValidity();
+		return false;
+	} else {
+		fileInput.setCustomValidity("");
+		return true;
+	}
 }
 
 function showModalDialog(title, message) {
---
+++ TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/product_form.js
@@ -0,0 +1,96 @@
+dropdownBrands = $("#brand");
+dropdownCategories = $("#category");
+
+$(document).ready(function () {
+
+	$("#shortDescription").richText();
+	$("#fullDescription").richText();
+
+	dropdownBrands.change(function () {
+		dropdownCategories.empty();
+		getCategories();
+	});
+	getCategories();
+	
+	$("input[name='extraImage']").each(function(index) {
+		$(this).change(function() {
+			showExtraImageThumbnail(this, index);
+		});
+	});
+});
+
+function showExtraImageThumbnail(fileInput, index) {
+	var file = fileInput.files[0];
+	var reader = new FileReader();
+	reader.onload = function (e) {
+		$("#extraThumbnail" + index).attr("src", e.target.result);
+	};
+	reader.readAsDataURL(file);
+
+	addNextExtraImageSection(index + 1);
+}
+
+function addNextExtraImageSection(index) {
+	htmlExtraImage = `
+		<div class="col border m-3 p-2" id="divExtraImages${index}">
+			<div id="extraImageHeader${index}"><label for="">Extra Image #${index + 1}: </label></div>
+			<div class="m-2">
+				<img id="extraThumbnail${index}" src="${defaultImageThumbnailSrc}" alt="Extra image #${index + 1}" class="img-fluid" >
+			</div>
+			<div class="">
+				<input type="file" 
+				onchange="showExtraImageThumbnail(this, ${index})"
+				name="extraImage"
+				 accept="image/png, image/jpeg" />
+			</div>
+		</div>`
+		;
+		
+	htmlLinkRemove = `
+		<a class="btn fas fa-times-circle fa-2x icon-dark float-right" 
+		title="Remove this image"
+		href="javascript:removeExtraImage(${index} - 1)" >
+		</a>
+		`;	
+	$("#divProductImages").append(htmlExtraImage);
+	$("#extraImageHeader" + (index - 1)).append(htmlLinkRemove);
+}
+
+function removeExtraImage(index) {
+	$("#divExtraImages" + index).remove();
+}
+
+
+function getCategories() {
+	brandId = dropdownBrands.val();
+	url = moduleURLBrands + "/" + brandId + "/categories";
+
+	$.get(url, function (responseJson) {
+		$.each(responseJson, function (index, category) {
+			$("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
+		});
+	});
+}
+
+function checkUnique(form) {
+	url = "[[@{/products/check_unique}]]";
+	productId = $("#id").val();
+	productName = $("#name").val();
+
+	csrfValue = $("input[name='_csrf']").val();
+	params = { id: productId, name: productName, _csrf: csrfValue };
+
+	$.post(url, params, function (response) {
+		if (response == "OK") {
+			form.submit();
+		} else if (response == "Duplicate") {
+			showModalDialog("Warning", "There is another product having the mail: " + productName);
+		} else {
+			showModalDialog("Error", "Unknown response from server");
+		}
+	}).fail(function () {
+		showModalDialog("Error", "Could not connect to the server");
+	});
+
+	return false;
+}
