026fa77915de299771e5df9815fd9492e74648ae
Huy Quang <huyquang271099@gmail.com>
2022-06-04 16:18:43 +0700

    add htmlLinkRemove  on product_detail use JS

--- TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/product_form_details.js
+++ TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/product_form_details.js
@@ -1,6 +1,10 @@
 function addNextDetailSection() {
+	
+	allDivDetails = $("[id^='divDetail']");
+	divDetailCount = allDivDetails .length;
+	
     htmlDetailSection = `
-         <div class="form-inline">
+         <div class="form-inline" id="divDetail${divDetailCount}">
 			<label for="" class="m-3">Name</label>
 			<input type="text" class="form-control w-25" name="detailNames" maxlength="255">
 			<label for="" class="m-3">Value</label>
@@ -9,4 +13,13 @@ function addNextDetailSection() {
     `;
 
     $("#divProductDetails").append(htmlDetailSection);
+    
+   htmlLinkRemove = `
+		<a class="btn fas fa-times-circle fa-2x icon-dark float-right" 
+		title="Remove this image"
+		</a>
+		`;
+		
+	previousDivDetailSection = allDivDetails.last();
+	previousDivDetailSection.append(htmlLinkRemove);
 }
