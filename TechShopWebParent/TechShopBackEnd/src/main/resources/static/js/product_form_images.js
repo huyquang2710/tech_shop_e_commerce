96b5697e3bb63c55a7d6c851e963ac91a3266a4e
Huy Quang <huyquang271099@gmail.com>
2022-06-04 15:32:03 +0700

    refactor code

--- TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/product_form_images.js
+++ TechShopWebParent/TechShopBackEnd/src/main/resources/static/js/product_form_images.js
@@ -1,17 +1,6 @@
 var extraImageCount = 0;
-dropdownBrands = $("#brand");
-dropdownCategories = $("#category");
 
 $(document).ready(function () {
-
-	$("#shortDescription").richText();
-	$("#fullDescription").richText();
-
-	dropdownBrands.change(function () {
-		dropdownCategories.empty();
-		getCategories();
-	});
-	getCategories();
 	
 	$("input[name='extraImage']").each(function(index) {
 		extraImageCount++;
@@ -63,38 +52,4 @@ function addNextExtraImageSection(index) {
 
 function removeExtraImage(index) {
 	$("#divExtraImages" + index).remove();;
-}
-
-
-function getCategories() {
-	brandId = dropdownBrands.val();
-	url = moduleURLBrands + "/" + brandId + "/categories";
-
-	$.get(url, function (responseJson) {
-		$.each(responseJson, function (index, category) {
-			$("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
-		});
-	});
-}
-
-function checkUnique(form) {
-	productId = $("#id").val();
-	productName = $("#name").val();
-
-	csrfValue = $("input[name='_csrf']").val();
-	params = { id: productId, name: productName, _csrf: csrfValue };
-
-	$.post(checkUniqueUrl, params, function (response) {
-		if (response == "OK") {
-			form.submit();
-		} else if (response == "Duplicate") {
-			showModalDialog("Warning", "There is another product having the mail: " + productName);
-		} else {
-			showModalDialog("Error", "Unknown response from server");
-		}
-	}).fail(function () {
-		showModalDialog("Error", "Could not connect to the server");
-	});
-
-	return false;
-}
+}	
